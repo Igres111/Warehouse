9f93bac50f907461595ba5c2b58d081431f7b2c0
Igres111 <sergikaralashvili@gmail.com>
2025-04-28 12:44:34 +0400

    Add project files.

---
+++ Warehouse/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+DotNetEnv.Env.Load();
+var connection = Environment.GetEnvironmentVariable("connection");
+var key = Environment.GetEnvironmentVariable("Key");
+if (string.IsNullOrEmpty(key))
+{
+    throw new Exception("JWT secret key is not set in the environment variables.");
+}
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
